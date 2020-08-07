e11bf04289cdae0ff2a397bcd82a281dd3bf7f37
DragooCho <66540054+DragooCho@users.noreply.github.com>
2020-08-07 12:53:17 +0900

    add

---
+++ wordrelay-js-work/index.js
@@ -0,0 +1,24 @@
+const btn = document.querySelector(".Btn-click");
+
+btn.addEventListener("click", () => {
+  const wordTag = document.querySelector(".wordStart");
+  const word = wordTag.textContent;
+  const inputTag = document.querySelector("#FirstInput");
+  const resultTag = document.querySelector("#result");
+  const input = inputTag.value;
+  const lastIndex = word.length - 1;
+  //word.length - 1 는 단어의 끝글자를 가져옴
+  const w = word[lastIndex];
+  const i = input[0];
+  if (w === i) {
+    wordTag.textContent = input;
+    resultTag.textContent = "정답";
+    inputTag.value = null;
+    inputTag.focus();
+  } else {
+    resultTag.textContent = "땡";
+    inputTag.value = null;
+    inputTag.focus();
+    // focus()는 입력창에 커서를 넣고 대기하라는것
+  }
+});
