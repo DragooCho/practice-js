99d2ae4a7262fa321efddcc6460aa2695954d6e2
DragooCho <66540054+DragooCho@users.noreply.github.com>
2020-08-11 13:00:47 +0900

    Update index.js

--- numberBaseball-js-work/index.js
+++ numberBaseball-js-work/index.js
@@ -29,23 +29,18 @@ const Btn = document.querySelector(".Btn-click");
 const resultLine = document.querySelector("#result");
 
 function MakeRandomNumber() {
-  const number1 = Math.floor(Math.random() * 10);
-  const number2 = Math.floor(Math.random() * 10);
-  const number3 = Math.floor(Math.random() * 10);
-  const number4 = Math.floor(Math.random() * 10);
-  if (number1 === number2) {
-    return MakeRandomNumber();
-  } else if (number2 === number3) {
-    return MakeRandomNumber();
-  } else if (number3 === number4) {
-    return MakeRandomNumber();
-  } else if (number4 === number1) {
-    return MakeRandomNumber();
-  } else if (number3 === number1) {
-    return MakeRandomNumber();
-  } else if (number2 === number4) {
-    return MakeRandomNumber();
-  } else if (9 === number4 || 8 === number4) {
+  const number1 = Math.floor(Math.random() * 10),
+    number2 = Math.floor(Math.random() * 10),
+    number3 = Math.floor(Math.random() * 10),
+    number4 = Math.floor(Math.random() * 10);
+  if (
+    number1 === number2 ||
+    number2 === number3 ||
+    number3 === number4 ||
+    number4 === number1 ||
+    number3 === number1 ||
+    number2 === number4
+  ) {
     return MakeRandomNumber();
   } else {
     answerNumber = [number1, number2, number3, number4];
