f1e452226d3ebac556ae769b52ee752fe020533d
DragooCho <66540054+DragooCho@users.noreply.github.com>
2020-08-14 22:29:33 +0900

    Update index.js

--- numberBaseball-js-work2/index.js
+++ numberBaseball-js-work2/index.js
@@ -4,22 +4,26 @@ const logs = document.querySelector("#logs");
 
 let numbers = [];
 let a = -1;
-while (a <= 8) {
-  const randomNumber = a + 1;
-  numbers.push(randomNumber);
-  a += 1;
+// while (a <= 8) {
+//   const randomNumber = a + 1;
+//   numbers.push(randomNumber);
+//   a += 1;
+// }
+for (let n = 0; n <= 9; n += 1) {
+  // 시작조건,반복이 멈추는 조건, 반복될때 추가되는 조건
+  numbers.push(n);
 }
 
-// console.log(numbers);
+console.log(numbers);
 
 let answer = [];
 
 for (let n = 0; n <= 3; n += 1) {
-  const index = Math.floor(Math.random() * (10 - n));
+  const index = Math.floor(Math.random() * numbers.length);
   answer.push(numbers[index]);
   numbers.splice(index, 1);
 }
-// 3046  4316
+
 console.log(answer);
 
 let count = 0;
@@ -28,6 +32,8 @@ check.addEventListener("click", () => {
   if (value && value.length === 4) {
     if (answer.join("") === value) {
       logs.appendChild(document.createTextNode("HR"));
+      // log.textContent = HR 은 기존 글자를 지우고 문장을 추가하는거면
+      // logs.appendChild(document.createTextNode("HR"));는 기존 문장을 유지하고 새문장을 추가함
     } else {
       console.log("다르다");
       let strike = 0;
@@ -43,9 +49,19 @@ check.addEventListener("click", () => {
           }
         }
       }
-      logs.appendChild(
-        document.createTextNode(`${input.value} : ${strike}strike ${ball}ball`)
+      const massage = document.createTextNode(
+        `${input.value} : ${strike}strike ${ball}ball`
       );
+      logs.appendChild(massage);
+      logs.appendChild(document.createElement("br"));
+      // 'br' 를 넣어 줄 바꿈을 추가하는 작업
+      if (count >= 9) {
+        logs.appendChild(
+          document.createTextNode(`Game Over : ${answer.join("")}`)
+        );
+      } else {
+        count += 1;
+      }
     }
   }
 });
